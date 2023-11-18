c90bc17ab6ff8bdc5b0078c1dd7fc62e3454c010
ccyykkcyk <91893497+ccyykkcyk@users.noreply.github.com>
2023-11-18 14:52:49 +0800

    c++20

--- solutions/daily/2342.h
+++ solutions/daily/2342.h
@@ -2,41 +2,40 @@
 #include <unordered_map>
 #include <vector>
 
-using ::std::vector, ::std::unordered_map;
+using ::std::vector, ::std::unordered_map, ::std::for_each;
 
 class Solution {
   public:
     int maximumSum(vector<int> &nums) {
-        unordered_map<int, vector<int>> map;
         int result = -1;
-        for (int i = 0; i < nums.size(); i++) {
-            map[getDigitsSum(nums[i])].push_back(nums[i]);
-        }
-        for (const auto &foo : map) {
+        auto digitsSum = [](int num) {
+            int sum = 0;
+            while (num) {
+                sum += num % 10;
+                num /= 10;
+            }
+            return sum;
+        };
+        auto maxTwoSum = [](const vector<int> &nums) {
+            int max1 = 0, max2 = 0;
+            for (auto num : nums) {
+                if (num > max1) {
+                    max2 = max1;
+                    max1 = num;
+                } else if (num > max2) {
+                    max2 = num;
+                }
+            }
+            return max1 + max2;
+        };
+        unordered_map<int, vector<int>> map;
+        for_each(nums.begin(), nums.end(),
+                 [&](auto num) { map[digitsSum(num)].push_back(num); });
+        for_each(map.begin(), map.end(), [&](const auto &foo) {
             if (foo.second.size() >= 2) {
-                result = std::max(getTopTwoSum(foo.second), result);
+                result = std::max(maxTwoSum(foo.second), result);
             }
-        }
+        });
         return result;
-    }
-    int getDigitsSum(int num) {
-        int sum = 0;
-        while (num) {
-            sum += num % 10;
-            num /= 10;
-        }
-        return sum;
-    }
-    int getTopTwoSum(const vector<int> &nums) {
-        int max1 = 0, max2 = 0;
-        for (const auto num : nums) {
-            if (num > max1) {
-                max2 = max1;
-                max1 = num;
-            } else if (num > max2) {
-                max2 = num;
-            }
-        }
-        return max1 + max2;
     }
 };
